546b5da6d53038bb37d0f4f29aead08f3fcf1a09
dayummson <quincyrodgemacalalag@gmail.com>
2025-09-14 12:36:25 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+});
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+}
